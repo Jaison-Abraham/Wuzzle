ab9f78cf45d062b1b294f68568f7b283e4c2e358
Jaison-Abraham <jaisonabraham8606@gmail.com>
2025-08-04 15:26:55 -0400

    UI Update

---
+++ Wuzzle/Wuzzle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wuzzle
+//
+//  Created by JAISON ABRAHAM on 2025-08-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
